d03f729a20959b42565e492b440e51e4b6037018
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-05-02 01:24:15 -0500

    Add Messaging Service Class

---
+++ src/main/java/com/mirai/importback/services/impl/MessagingService.java
@@ -0,0 +1,43 @@
+package com.mirai.importback.services.impl;
+
+import com.mirai.importback.entities.Message;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+
+public class MessagingService {
+
+    private ArrayList<Message> messages;
+
+    public MessagingService() {
+        this.messages = new ArrayList<Message>();
+    }
+
+    public void sendMessage(String from, String messageContent) {
+        Message message = new Message(from, LocalDateTime.now(), messageContent);
+        this.messages.add(message);
+    }
+
+    public Message filterMessageByContent(String content) {
+        for (Message message : this.messages) {
+            if (message.getMessageContent().contains(content)) {
+                return message;
+            }
+        }
+        return null;
+    }
+
+    public ArrayList<Message> filterMessagesByName(String name) {
+        ArrayList<Message> filteredMessages = new ArrayList<Message>();
+        for (Message message : this.messages) {
+            if (message.getFrom().contains(name)) {
+                filteredMessages.add(message);
+            }
+        }
+        return filteredMessages;
+    }
+
+    public ArrayList<Message> getMessages() {
+        return this.messages;
+    }
+}
