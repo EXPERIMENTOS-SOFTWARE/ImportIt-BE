5b9c9cff71e4346a24dd73394940488404dd1fd8
Gonzalo Barrazueta <u20191a972@upc.edu.pe>
2023-05-02 02:20:39 -0500

    Update test

--- src/test/java/unitTests/UpdateOrderStepsServiceTest.java
+++ src/test/java/unitTests/UpdateOrderStepsServiceTest.java
@@ -13,20 +13,19 @@ class UpdateOrderStepsServiceTest {
     StatusStep steps;
 
     UpdateOrderStepsServiceTest() {
-        this.order = new Orders("", 0);
+        this.order = new Orders("PENDING", 1);
         this.sendOrderUpdateService = new UpdateOrderStepsService();
         this.steps = new StatusStep();
     }
 
     @Test
     void testOrderIsPending() {
-        this.sendOrderUpdateService.updateOrderStatus(this.order);
         Assertions.assertEquals("PENDING", this.sendOrderUpdateService.getStatusMessage(this.order));
     }
 
     @Test
     void testOrderStepsFlow() {
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 2; i <= 5; i++) {
             this.sendOrderUpdateService.updateOrderStatus(this.order);
             Assertions.assertEquals(this.steps.getSteps().get(i), this.sendOrderUpdateService.getStatusMessage(this.order));
         }
